524f8da684a02c27c3c9f1813a0013455b8340db
geraliaga@aulanz.net <germany@MacBook-Pro.local>
2020-07-14 01:41:11 +0200

    pagina

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie-edge">
+	<link rel="stylesheet"  type="text/css" href="css/bootstrap.css">
+  	<link rel="stylesheet"  type="text/css" href="css/style.css">
+  	<link rel=”shortcut icon” type=”image/png” href=”img/favicon.png”/>
+   	<link rel="stylesheet"  type="text/css" href="css/alertify.min.css">
+	<!-- <link rel="stylesheet"  type="text/css" href="css/bootstrap.min.css"> -->
+	<title>Document</title>
+</head>
+<body>
+  
+
+  
+
+	 <script src="js/jquery-3.4.1.min.js"></script>
+	 <script src="js/popper.min.js"></script>
+	 <script src="js/bootstrap.min.js"></script>
+  	 <script src="js/alertify.min.js"></script>
+   	 <script src="alertify/alertify.min.js"></script>
+   	 <script src="moment/moment-with-locales.js"></script>
+	 <script>
+	 	$( document ).ready(function() {
+ 	 	console.log("Are /you READY!!");
+      
+   		 });
+ 
+	</script>
+  <script src="https://kit.fontawesome.com/a03bedb3c1.js" crossorigin="anonymous"></script>
+</body>
+</html>
